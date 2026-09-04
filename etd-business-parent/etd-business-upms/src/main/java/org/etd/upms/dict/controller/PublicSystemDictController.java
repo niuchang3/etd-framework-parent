c17af4fefd0a578282ca54a5c6872efdb5179371
牛昌 <Leo@ffff.team>
2026-09-04 14:03:29 +0800

    feature:字典接口免鉴权

--- etd-business-parent/etd-business-upms/src/main/java/org/etd/upms/dict/controller/PublicSystemDictController.java
+++ etd-business-parent/etd-business-upms/src/main/java/org/etd/upms/dict/controller/PublicSystemDictController.java
@@ -1,23 +1,16 @@
 package org.etd.upms.dict.controller;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.etd.framework.starter.client.core.permission.annotation.Permission;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.*;
 import org.etd.framework.common.core.model.ResultModel;
 import org.etd.upms.dict.biz.SystemDictBizService;
-import org.etd.upms.dict.controller.dto.SystemDictTypeSaveDTO;
 import org.etd.upms.dict.controller.vo.SystemDictDataVO;
 import org.etd.upms.dict.controller.vo.SystemDictTypeVO;
 import org.etd.upms.dict.service.SystemDictDataService;
 import org.etd.upms.dict.service.SystemDictTypeService;
-import org.etd.upms.menu.constant.MenuPermissionCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * 字典类型管理 Controller 控制器入口。
@@ -25,7 +18,7 @@ import java.util.Map;
 @Validated
 @RestController
 @RequestMapping("/v1/dict")
-public class PublicSystemDictTypeController {
+public class PublicSystemDictController {
 
     @Autowired
     private SystemDictTypeService dictTypeService;
